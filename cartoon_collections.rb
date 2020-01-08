a95fa1f90ab86c033fe1071a4aa8dbb18c46dd54
alexander diaz <alexanderjdiaz89@gmail.com>
2020-01-08 03:49:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,11 @@ def long_planeteer_calls(array)
 end
 
  def find_the_cheese(array)
- cheese_types = ["cheddar", "gouda", "camembert"]
-
-
-
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+   cheese_types.include?(cheese)
+    end 
+  end 
 # should user .incule? to solve
 
 
